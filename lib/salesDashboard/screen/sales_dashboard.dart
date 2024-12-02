07190d4f576411f8a948c2f66dd62a28bd2ed9ea
prakashbmdigital <prakeshkumar0580@gmail.com>
2024-12-02 15:40:38 +0530

    Extraction Filter code

--- lib/salesDashboard/screen/sales_dashboard.dart
+++ lib/salesDashboard/screen/sales_dashboard.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:siddha_connect/salesDashboard/component/radio.dart';
 import 'package:siddha_connect/utils/common_style.dart';
-import 'package:siddha_connect/utils/message.dart';
 import '../../utils/cus_appbar.dart';
 import '../../utils/drawer.dart';
 import '../../utils/providers.dart';
@@ -12,54 +11,11 @@ import '../component/dashboard_small_btn.dart';
 import '../component/date_picker.dart';
 import '../component/sales_data_show.dart';
 import '../component/dashboard_full_btn.dart';
-
-// class SalesDashboard extends ConsumerWidget {
-//   final String initialOption;
-//   const SalesDashboard({
-//     super.key,
-//     this.initialOption = 'MTD',
-//   });
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     WidgetsBinding.instance.addPostFrameCallback((_) {
-//       ref.read(selectedOption1Provider.notifier).state = initialOption;
-//     });
-//     // this provider call for dealer info
-//     final dealerInfo = ref.watch(dealerProvider);
-//     return PopScope(
-//       canPop: false,
-//       onPopInvoked: (didPop) {
-//         SystemNavigator.pop();
-//       },
-//       child: const Scaffold(
-//         backgroundColor: AppColor.whiteColor,
-//         drawer: CusDrawer(),
-//         appBar: CustomAppBar(),
-//         body: SingleChildScrollView(
-//           child: Column(
-//             children: [
-//               TopRadioButtons(),
-//               DatePickerContainer(),
-//               SalesDashboardCard(),
-//               SmallCusBtn(),
-//               FullSizeBtn()
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'dart:io'; // Import required for exit(0)
 
 class SalesDashboard extends ConsumerWidget {
   final String initialOption;
-  const SalesDashboard({
-    super.key,
-    this.initialOption = 'MTD',
-  });
+  const SalesDashboard({super.key, this.initialOption = 'MTD'});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -78,8 +34,9 @@ class SalesDashboard extends ConsumerWidget {
         if (lastBackPressTime == null ||
             now.difference(lastBackPressTime!) > const Duration(seconds: 2)) {
           lastBackPressTime = now;
-
-          Fluttertoast.showToast(msg: "Press back again to exit");
+          Fluttertoast.showToast(
+            msg: "Press back again to exit",
+          );
           return false;
         }
         exit(0);
@@ -103,6 +60,8 @@ class SalesDashboard extends ConsumerWidget {
     );
   }
 }
+
+
 
 class YTDSalesDashboard extends ConsumerWidget {
   final String initialOption;
